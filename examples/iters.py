fea449ff94874d1e83148d07e6db83fec92aac41
Florian Wechsung <f.wechsung@gmail.com>
2019-07-07 14:44:25 +0100

    added ldc3d problem

--- examples/iters.py
+++ examples/iters.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 from ldc2d.ldc2d import TwoDimLidDrivenCavityProblem
+from ldc3d.ldc3d import ThreeDimLidDrivenCavityProblem
 from bfs2d.bfs2d import TwoDimBackwardsFacingStepProblem
 from alfi import get_default_parser, get_solver, run_solver
 import os
@@ -10,12 +11,16 @@ parser.add_argument("--problem", type=str, required=True,
 parser.add_argument("--diagonal", type=str, default="left",
                     choices=["left", "right", "crossed"])
 parser.add_argument("--mesh", type=str)
+parser.add_argument("--nref-start", type=int)
+parser.add_argument("--nref-end", type=int)
 args, _ = parser.parse_known_args()
 
 if args.problem == "ldc2d":
     problem = TwoDimLidDrivenCavityProblem(args.baseN, args.diagonal)
 elif args.problem == "bfs2d":
     problem = TwoDimBackwardsFacingStepProblem(args.mesh)
+elif args.problem == "ldc3d":
+    problem = ThreeDimLidDrivenCavityProblem(args.baseN)
 else:
     raise NotImplementedError
 
@@ -24,9 +29,9 @@ end = 10000
 step = 100
 res = [1, 10, 100] + list(range(start, end+step, step))
 if args.problem == "bfs2d":
-    res = list(sorted(res + [150, 250, 350]))
+    res = list(sorted(res + [50, 150, 250, 350]))
 results = {}
-nrefs = range(1, args.nref+1)
+nrefs = range(args.nref_start, args.nref_end+1)
 tableres = [i for i in [10, 100, 1000, 5000, 10000] if i <= max(res)]
 dofs = {}
 for nref in nrefs:
