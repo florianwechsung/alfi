a971cfd73a6e7532b0a008f574896286c862c68b
Evangelia Antonopoulou <antonopoulou@maths.ox.ac.uk>
2021-02-01 10:09:40 +0000

    Remove duplicated directory

--- transient/alfi/__init__.py
+++
@@ -1,6 +0,0 @@
-from alfi.problem import NavierStokesProblem
-from alfi.solver import ScottVogeliusSolver, ConstantPressureSolver
-from alfi.relaxation import *
-from alfi.transfer import *
-from alfi.bary import *
-from alfi.driver import *
--- transient/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-setup(name="Alfi",
-      version="0.0.1",
-      description="Augmented Lagrangian based solver for the stationary incompressible Navier-Stokes equations",
-      packages = ["alfi"])
